4a5fdf5e1c571367da8dcee086127e0345247dde
Mary <marurb557@gmail.com>
2021-08-31 23:14:06 +0300

    Исправила одну ошибочку

--- Lesson 2/proga3.go
+++ Lesson 2/proga3.go
@@ -14,12 +14,11 @@ func main() {
 	sum := num1 + num2
 	dif := num1 - num2
 	mult := num1 * num2
-	var delim float64
-	delim = float64(num1 / num2)
+	var delim float64 = float64(num1 / num2)
 	ost := num1 % num2
 	fmt.Println("Таки я сложила ваши два числа и получилось: ", sum)
 	fmt.Println("Таки я из первого числа вычла второе и получилось: ", dif)
 	fmt.Println("Таки я умножила ваши два числа и получилось: ", mult)
-	fmt.Println("Таки я разделила первое число на второе получилось: ", delim)
+	fmt.Printf("Таки я разделила первое число на второе получилось: %.2f\n", delim)
 	fmt.Println("Таки я разделил первое число на второе и остаток получился: ", ost)
 }
