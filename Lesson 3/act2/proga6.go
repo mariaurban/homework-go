202481415114421e53a8762684cc05236aaaffb8
Mary <marurb557@gmail.com>
2021-09-15 03:30:11 +0300

    Доделала switch)

--- Lesson 3/act2/proga6.go
+++ Lesson 3/act2/proga6.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("сложна, потом доделаю")
+	var num string
+	fmt.Print("Чтобы выйти на связь с оператором нашмите 6 ")
+	fmt.Scanln(&num)
+	switch num {
+	case "6":
+		fmt.Println("Переводим вас на оператора. Ожидайте. Ваш номер в очереди 1488")
+	default:
+		fmt.Printf("Не правильное число")
+	}
 }
