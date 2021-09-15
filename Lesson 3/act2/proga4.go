ddee4bd2f34f139cb12f27361682f8b4b62149d7
MaryUrban <maria.urban@kupibilet.ru>
2021-09-15 12:23:54 +0300

    Исправление 4го задания с координатами

--- Lesson 3/act2/proga4.go
+++ Lesson 3/act2/proga4.go
@@ -10,26 +10,29 @@ func main() {
 	fmt.Println("Введи координату y: ")
 	fmt.Scanln(&y)
 
-	if y > 0 {
-		if x > 0 {
-			fmt.Println("Вы в верхней правой плоскости")
-		} else {
-			fmt.Println("Вы в нижней правой плоскости")
-			if x == 0 {
-				fmt.Println("Вы на оси X")
-			}
-		}
+	if y == 0 && x == 0 {
+		fmt.Println("Вы в начале координат")
 	} else {
-		if x > 0 {
-			fmt.Println("Вы в верхней левой плоскости")
+		if x == 0 {
+			fmt.Println("Вы на оси X")
 		} else {
-			fmt.Println("Вы в нижней левой плоскости")
-		}
-		if y == 0 {
-			fmt.Println("Вы на оси Y")
-		}
-		if y == 0 && x == 0 {
-			fmt.Println("Вы в начале координат")
+			if y == 0 {
+				fmt.Println("Вы на оси Y")
+			} else {
+				if y > 0 {
+					if x > 0 {
+						fmt.Println("Вы в верхней правой плоскости")
+					} else {
+						fmt.Println("Вы в нижней правой плоскости")
+					}
+				} else {
+					if x > 0 {
+						fmt.Println("Вы в верхней левой плоскости")
+					} else {
+						fmt.Println("Вы в нижней левой плоскости")
+					}
+				}
+			}
 		}
 	}
 }
