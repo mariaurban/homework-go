29da12e19b9ae196645259b762c84c7d13c501e1
MaryUrban <maria.urban@kupibilet.ru>
2021-09-15 17:52:46 +0300

    Исправление 3-го задания Ну теперь точно все

--- Lesson 3/act2/proga3.go
+++ Lesson 3/act2/proga3.go
@@ -6,6 +6,8 @@ func main() {
 	var a int
 	var b int
 	var c int
+	var between int
+
 	fmt.Println("Введи первое число: ")
 	fmt.Scanln(&a)
 	fmt.Println("Введи второе число: ")
@@ -13,25 +15,24 @@ func main() {
 	fmt.Println("Введи третье число: ")
 	fmt.Scanln(&c)
 
-	if (a == b) && (b == c) {
-		fmt.Println("Среднее число :", a)
-	} else {
-		if (a == b) || (a == c) {
-			fmt.Println("Среднее число :", a)
+	if (a > b) && (a < b) {
+		if c > b {
+			between = c
 		} else {
-			if (b == c) || (b == a) {
-				fmt.Println("Среднее число :", b)
-			} else {
-				if ((a < b) && (b < c)) || ((c < b) && (b < a)) {
-					fmt.Println("Среднее число:", b)
-				} else {
-					if ((b < a) && (a < c)) || ((c < a) && (a < b)) {
-						fmt.Println("Среднее число:", a)
-					} else {
-						fmt.Println("Среднее число:", c)
-					}
-				}
-			}
+			between = b
+		}
+	} else if (b > a) && (b > c) {
+		if a > c {
+			between = a
+		} else {
+			between = c
+		}
+	} else if (c > a) && (c > b) {
+		if a > b {
+			between = a
+		} else {
+			between = b
 		}
 	}
+	fmt.Println("Среднее число:", between)
 }
