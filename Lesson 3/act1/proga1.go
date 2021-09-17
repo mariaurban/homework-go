232eae518c165874f0ac40d0891297edba655516
MaryUrban <maria.urban@kupibilet.ru>
2021-09-17 12:16:22 +0300

    Pro Max Next Level

--- Lesson 3/act1/proga1.go
+++ Lesson 3/act1/proga1.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	var asciiNum int
-	fmt.Println("Здарова, человек. Введи число ПОЛОЖИТЕЛЬНОЕ ЦЕЛОЕ ЧИСЛО:")
-	fmt.Scanln(&asciiNum)
-	for asciiNum < 0 {
-		fmt.Println("Ах ты плохой человек, это не положительное число5")
-		fmt.Println("Введи число ПОЛОЖИТЕЛЬНОЕ ЦЕЛОЕ ЧИСЛО")
-		fmt.Println("Желательно более 33:")
-		fmt.Scanln(&asciiNum)
-	}
-
-	character := string(asciiNum)
-	fmt.Println(asciiNum, " : ", character)
 
 }
--- Lesson 3/act1/proga5.go
+++ Lesson 3/act1/proga5.go
@@ -1,3 +1,6 @@
+// Задача :
+// Посчитать сумму цифр числа в десятичной записи
+
 package main
 
 import (
@@ -5,8 +8,10 @@ import (
 )
 
 func main() {
-	var number int
-	fmt.Println("Пользователь введи число:")
-	fmt.Scanln(&number)
-	fmt.Printf("%04b\n", number)
+	var number1 int
+	var number2 int
+	fmt.Println("Пользователь введи первое число:")
+	fmt.Scanln(&number1)
+	fmt.Println("Пользователь введи второе число:")
+	fmt.Scanln(&number2)
 }
