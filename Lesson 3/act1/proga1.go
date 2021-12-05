e57fe89951a74b2be0ec187fc627526ce1793af4
Mary <marurb557@gmail.com>
2021-12-05 18:03:25 +0300

    я что-то тут удаляла

--- Lesson 3/act1/proga1.go
+++ Lesson 3/act1/proga1.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func main() {
+	var numberDvo string
+	fmt.Println("Пользователь введи десятичное число:")
+	fmt.Scanln(&numberDvo)
+	number, err := strconv.ParseUint(numberDvo, 2, 32)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(number)
 }
